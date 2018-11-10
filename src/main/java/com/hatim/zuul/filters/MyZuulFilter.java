1e8740b243e04ff4147c82d89b8f3ebb52a62665
Hatim <hatim@mypc.com>
2018-11-10 07:47:27 +0000

    initial commit

---
+++ src/main/java/com/hatim/zuul/filters/MyZuulFilter.java
@@ -0,0 +1,34 @@
+package com.hatim.zuul.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyZuulFilter extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyZuulFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        logger.info("Received request {}", RequestContext.getCurrentContext().getRequest().getRequestURL());
+        return null;
+    }
+}
